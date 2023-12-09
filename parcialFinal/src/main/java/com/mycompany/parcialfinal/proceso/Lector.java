46ed41b9ee77c7160c67e8c055b0e3464d5631c2
Juan José Hurtado <142424907+JuanHurtadom@users.noreply.github.com>
2023-12-09 14:08:56 -0500

    Update Lector.java

--- parcialFinal/src/main/java/com/mycompany/parcialfinal/proceso/Lector.java
+++ parcialFinal/src/main/java/com/mycompany/parcialfinal/proceso/Lector.java
@@ -17,21 +17,47 @@ public class Lector {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.direccion = direccion;
+        this.copias = new ArrayList<>();
     }
-
-    
-    
-    
-    
+          
     public void addCopia(Copia copia) {
-        if (copias.size() < 3) {
-            copias.add(copia);
+        if (this.copias.size() < 3) {
+            this.copias.add(copia);
         } else {
             System.out.println("No se pueden añadir más copias. El lector ya tiene 3 copias.");
         }
     }
-    
+
+    public void removerCopia(Copia copia) {
+        copias.remove(copia);
+    }
     public void comprobarMultasPendientes(){
-        
+    }
+    public String getNombreCompleto(){
+        return nombre + " " + apellidos;
+    }
+
+    public int getNumSocio() {
+        return numSocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public ArrayList<Copia> getCopias() {
+        return copias;
+    }
+
+    public Multa getMulta() {
+        return multa;
     }
 }
