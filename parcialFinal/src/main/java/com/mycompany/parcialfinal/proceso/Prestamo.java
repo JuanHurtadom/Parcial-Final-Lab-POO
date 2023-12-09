d7a5c5d69fdfcfac2316cd272a3daabb6b6a2d24
Juan José Hurtado <142424907+JuanHurtadom@users.noreply.github.com>
2023-12-09 14:16:15 -0500

    Update Prestamo.java

--- parcialFinal/src/main/java/com/mycompany/parcialfinal/proceso/Prestamo.java
+++ parcialFinal/src/main/java/com/mycompany/parcialfinal/proceso/Prestamo.java
@@ -2,23 +2,30 @@
 package com.mycompany.parcialfinal.proceso;
 
 import java.util.Date;
-
+import java.util.Calendar;
 
 public class Prestamo {
+
+    private static final int CONST_PLAZO_DEVOLVER = 30; //dias
+    
     private Date fechaInicio;
     private Date fechaFin;
     private Multa multa;
 
-    public Prestamo(Date fechaInicio, Date fechaFin) {
+    public Prestamo(Date fechaInicio) {
         this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
     }
     
-    public void calcularFechaFin(){
-        
+    public Date calcularFechaFin(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fechaInicio);
+        calendar.add(Calendar.DAY_OF_MONTH, CONST_PLAZO_DEVOLVER);
+        return calendar.getTime();
     }
     
-    public void generarMulta(){
-        
+    public Multa generarMulta(){
+        Multa nuevaMulta = new Multa(new Date());
+        this.multa = nuevaMulta;
+        return nuevaMulta;
     }
 }
